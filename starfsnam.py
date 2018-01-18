0de89bd4e9b056dd93fede1fb07104194a67f6b2
viktoriaomars <viktoriaomars@gmail.com>
2018-01-18 14:46:36 +0000

    Inital commit

---
+++ starfsnam.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 11 15:08:35 2018
+
+@author: Viktoria
+"""
+
+id2gt = {} # Dictionary/hash table
+samples2index = {}
+four_samples = []
+
+with open("test.vcf", "r") as f:
+    for line in f:
+        if line.startswith("##"):
+            continue
+        elif line.startswith("#"):
+            header = line.rstrip().split("\t")
+            for s in range(9, len(header)):
+                samples2index[header[s]] = s-9
+                
+            #print(samples2index)
+            
+        #print (line.rstrip().split("\t"))
+        
+        
+        columns = line.split("\t")
+        variant_id = columns[2]
+        id2gt[variant_id] = [0]*(len(columns) - 9)
+        
+        for s in range(9, len(columns)):
+            gt=columns[s].split(":")[0]
+            id2gt[variant_id][s-9] = gt.count("1")
+            
+        #print (id2gt)
+        
+with open("test.four", "r") as g:
+    four_zeros = [0, 0, 0]
+    four_ones = [0, 0, 0]
+    four_twos = [0, 0, 0]
+    
+    for line in g:
+        four_columns = line.rstrip().split("\t")
+        sample_no = four_columns[0]
+        variant = four_columns[1]
+        gt1 = four_columns[2]
+        gt2 = four_columns[3]
+        
+        
+        if sample_no not in samples2index:
+            continue
+        
+        if variant not in id2gt:
+            continue
+        
+        sample_index = samples2index[sample_no]
+        variant_gt = id2gt[variant]
+        
+
+        if ((gt1 == '0') or (gt1 == '1')) and ((gt2 == '0') or (gt2 == '1')):
+            gt1 = int(gt1)
+            gt2 = int(gt2)
+            
+            if (gt1 + gt2) == 0:
+                four_zeros[variant_gt[sample_index]] +=1
+                #print(four_zeros)
+            elif (gt1 + gt2) == 1:
+                four_ones[variant_gt[sample_index]] +=1
+                #print(four_ones)
+            else:
+                four_twos[variant_gt[sample_index]] +=1
+                #print(four_twos)
+            
+            #er variant_gt sama og gt1 og gt2
+            if variant_gt[sample_index] == (gt1 + gt2):
+                print(sample_no, "is the same")
+            else:
+                print("Mismatch on", variant, "in", sample_no)
+        else:
+            print("Data missing")
+                     
+        
+        
+print("  ", "0", "1", "2", sep = "  ")
+print("0:", four_zeros)
+print("1:", four_ones)
+print("2:", four_twos)
